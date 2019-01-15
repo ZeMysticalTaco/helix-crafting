219bb5038b7951177bff54714f496d941cde868e
Unknown <zemysticaltaco@gmail.com>
2019-01-15 17:08:04 -0600

    User interface overhaul

--- crafting/cl_plugin.lua
+++ crafting/cl_plugin.lua
@@ -1,31 +1,72 @@
+surface.CreateFont("WastelandTiny", {
+	font = "Bahnschrift SemiLight Condensed", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 13,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false
+})
+
+surface.CreateFont("WastelandMedium", {
+	font = "Bahnschrift SemiLight Condensed", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 26,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false
+})
+
+surface.CreateFont("WastelandStandard", {
+	font = "Bahnschrift SemiLight Condensed", -- Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 35,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false
+})
+
 local PANEL = {}
 
 function PANEL:Init()
+	self.infolabel = self:Add("DLabel")
+	self.infolabel:Dock(TOP)
+	self.infolabel:SetContentAlignment(5)
+	self.infolabel:SetExpensiveShadow(1, Color(0, 0, 0, 255))
+	self.infolabel:SetText("Hover over the icon of an item to get more information about it.")
+	self.infolabel:SetFont("WastelandMedium")
 	ix.gui.crafting = self
-	self:SetSize(ScrW() / 2.8, ScrH() / 2)
+	self:SetSize(ScrW() / 2, ScrH() / 1.5)
 	self:Center()
-	self.infotext = self:Add("DLabel")
-	self.infotext:Dock(TOP)
-	self.infotext:SetContentAlignment(5)
-	self.infotext:SetText("Hover over a recipe's icon to get information on that recipe, click it to attempt to craft it.")
-	self.infotext:SetFont("ixSmallFont")
-	self.infotext:SetExpensiveShadow(1)
-
-	self.infotext.Paint = function(this, w, h)
-		surface.SetDrawColor(0, 0, 0, 75)
-		surface.DrawRect(0, 0, w, h)
-	end
-
-	--self:MakePopup()
-	--self:SetTitle("Crafting")
-	self.CraftingList = self:Add("DPanelList")
+	self.CraftingList = self:Add("DScrollPanel")
 	self.CraftingList:Dock(FILL)
-	self.CraftingList:SetSpacing(2)
-	self.CraftingList:SetPadding(5)
 	local w, h = self:GetSize()
-	self.CraftingList:DockMargin(0, 0, 0, 0)
-	self.CraftingList:EnableHorizontal(true)
-	self.CraftingList:EnableVerticalScrollbar(true)
 
 	for k, v in pairs(STORED_RECIPES) do
 		--printTable(v)
@@ -34,85 +75,122 @@ function PANEL:Init()
 
 			--print(v["blueprint"])
 			if table.HasValue(data, v["blueprint"]) then
-				local item = self:AddItem(v["name"], v["model"], v["desc"], v["req"], v["results"], v["skill"], v["blueprint"],v["guns"], v["entity"])
+				local item = self:AddItem(v["name"], v["model"], v["desc"], v["req"], v["results"], v["skill"], v["blueprint"], v["guns"], v["entity"])
 				item.id = v["id"]
 			end
 		end
+
 		if not v["blueprint"] then
 			local item = self:AddItem(v["name"], v["model"], v["desc"], v["req"], v["results"], v["skill"], v["guns"], v["entity"])
 			item.id = v["id"]
 		end
-		
 	end
 end
 
 function PANEL:AddItem(name, icon, desc, req, results, skill, blueprint, guns, entity)
-	--[[-------------------------------------------------------------------------
-	Structure:
-	1: Item Name
-	2: Icon
-	3: Description
-	4: Requirements
-	5: Results
-	---------------------------------------------------------------------------]]
-	self.test = self.CraftingList:Add("ixCraftingItem")
+	self.test = self:Add("CraftingListItem")
 	self.test:SetItem(name, icon, desc, req, results, skill, blueprint or false, guns or false, entity or false)
-	local recipe = self.CraftingList:AddItem(self.test)
+	self.CraftingList:AddItem(self.test)
+	--self.test:DockMargin(0,0,0,5)
+
 	return self.test
 end
 
-vgui.Register("ixCrafting", PANEL, "DPanel")
+vgui.Register("CraftingListFrame", PANEL, "DPanel")
 local PANEL = {}
 
 function PANEL:Init()
-	self:SetSize(128, 128)
-	self.Icon = self:Add("SpawnIcon")
-	self.Icon:SetModel("models/player.mdl")
-	self.Icon:SetSize(72, 72)
-	self.Icon:Center()
-	self.button = self:Add("DButton")
-	self.button:SetSize(self:GetWide(), self:GetTall())
-	self.button.Paint = function() end
-	self.button:SetText("")
+	local size = self:GetParent():GetSize()
+	self:SetWide(size / 2)
+	self:SetTall(size / 8)
+	self:Dock(TOP)
+	local w, h = self:GetSize()
+	self.spawnicon = self:Add("SpawnIcon")
+	self.spawnicon:SetSize(72, 72)
+	self.spawnicon:SetPos(5, 5)
+	self:SetModel("icon")
+	self.labelitem = self:Add("DLabel")
+	self.labelitem:Dock(TOP)
+	self.labelitem:SetFont("WastelandStandard")
+	self.labelitem:SetText("")
+	self.labelitem:SetAutoStretchVertical(true)
+	self.labelitem:DockMargin(120, 0, 0, 0)
+	self.labelitemdesc = self:Add("DLabel")
+	self.labelitemdesc:Dock(TOP)
+	self.labelitemdesc:SetFont("WastelandMedium")
+	self.labelitemdesc:SetText("")
+	self.labelitemdesc:SetAutoStretchVertical(true)
+	self.labelitemdesc:DockMargin(120, 0, 0, 0)
+	self.labelitemreq = self:Add("DLabel")
+	self.labelitemreq:Dock(TOP)
+	self.labelitemreq:SetFont("WastelandMedium")
+	self.labelitemreq:SetText("")
+	self.labelitemreq:SetAutoStretchVertical(true)
+	self.labelitemreq:DockMargin(120, 0, 0, 0)
+	self.craftbutton = self:Add("DButton")
+	self.craftbutton:SetText("")
+	self.craftbutton:AlignRight(-365)
+	self.craftbutton:AlignTop(20)
+	self.craftbutton:SetSize(150, 40)
 
-	function self.button:DoClick()
-		local parent = self:GetParent()
-		netstream.Start("ixCraftItem", {parent.id})
+	function self.craftbutton:Paint(w2, h2)
+		local col
+
+		if self:GetParent():HasRequirements() then
+			col = Color(0, 150, 0, 150)
+		else
+			col = Color(75, 75, 75, 150)
+		end
+
+		surface.SetDrawColor(col)
+		surface.DrawRect(0, 0, w2, h2)
+		surface.SetDrawColor(0, 0, 0, 255)
+		surface.DrawOutlinedRect(0, 0, w2, h2)
+		draw.SimpleTextOutlined("Craft", "WastelandStandard", w2 - 100, h2 / 18, Color(255, 255, 255, 255), 5, 5, 1, Color(0, 0, 0, 255))
 	end
 
-	self.name = self:Add("DLabel")
-	self.name:Dock(BOTTOM)
-	self.name:SetText("Piece of Cloth")
-	self.name:SetContentAlignment(5)
-	self.name:SetTextColor(color_white)
-	self.name:SetFont("ixSmallFont")
-	self.name:SetExpensiveShadow(1, Color(0, 0, 0, 200))
+	function self.craftbutton:DoClick()
+		surface.PlaySound("UI/buttonclick.wav")
 
-	self.name.Paint = function(this, w, h)
-		surface.SetDrawColor(0, 0, 0, 75)
-		surface.DrawRect(0, 0, w, h)
+		timer.Simple(SoundDuration("UI/buttonclick.wav") - 0.1, function()
+			surface.PlaySound("UI/buttonclickrelease.wav")
+		end)
+		local parent = self:GetParent()
+		print(parent)
+		print(parent.id)
+		netstream.Start("ixCraftItem", {parent.id})
 	end
 end
 
 function PANEL:SetItem(name, icon, desc, req, results, skill, blueprint, guns, entity)
-	self.name:SetText(name)
-	self.Icon:SetModel(icon)
+	--creating identifiers
+	self.labelitem:SetText(name)
+	self.spawnicon:SetModel(icon)
 	self.description = desc
+	self.labelitemdesc:SetText(desc)
 	self.requirements = req
+	local realreqd = {}
+
+	for k, v in pairs(self.requirements) do
+		local item = ix.item.Get(k)
+		realreqd[#realreqd + 1] = item.name .. " (" .. v .. "x)"
+	end
+
+	self.labelitemreq:SetText("Requirements: " .. table.concat(realreqd, ", "))
 	self.results = results
 	self.skill = skill
 	self.blueprint = blueprint
 	self.guns = guns
 	self.entity = entity
-
-	self.button:SetHelixTooltip(function(tooltip)
+	--self.item = ix.items.list[item]
+	self.spawnicon:SetHelixTooltip(function(tooltip)
 		local title = tooltip:AddRow("title")
 		title:SetImportant()
-		title:SetText(self.name:GetText())
+		title:SetText(self.labelitem:GetText())
 		title:SetBackgroundColor(ix.config.Get("color"))
 		title:SizeToContents()
 		local description = tooltip:AddRow("description")
-		description:SetText(self.description)
+		description:SetText(self.labelitemdesc:GetText())
 		description:SizeToContents()
 		local requirements = tooltip:AddRow("requirements")
 		local realreq = {}
@@ -139,6 +217,7 @@ function PANEL:SetItem(name, icon, desc, req, results, skill, blueprint, guns, e
 		else
 			requirements:SetBackgroundColor(derma.GetColor("Success", tooltip))
 		end
+
 		--requirements:SizeToContents()
 		if #realreq >= 4 then
 			requirements:SizeToContents()
@@ -146,6 +225,7 @@ function PANEL:SetItem(name, icon, desc, req, results, skill, blueprint, guns, e
 		else
 			requirements:SizeToContents()
 		end
+
 		local results = tooltip:AddRow("results")
 		local realres = {}
 
@@ -167,7 +247,6 @@ function PANEL:SetItem(name, icon, desc, req, results, skill, blueprint, guns, e
 
 			skill:SetText("Required Skills: " .. table.concat(skillist, ", "))
 			skill:SizeToContents()
-
 			local skillslist = {}
 
 			for k, v in pairs(self.skill) do
@@ -175,6 +254,7 @@ function PANEL:SetItem(name, icon, desc, req, results, skill, blueprint, guns, e
 					skillslist[#skillslist + 1] = k
 				end
 			end
+
 			if #skillslist > 0 then
 				skill:SetBackgroundColor(derma.GetColor("Error", tooltip))
 			else
@@ -193,12 +273,38 @@ function PANEL:SetItem(name, icon, desc, req, results, skill, blueprint, guns, e
 			bp:SizeToContents()
 		end
 	end)
+
 end
 
-vgui.Register("ixCraftingItem", PANEL, "DPanel")
+function PANEL:Paint(w, h)
+	surface.SetDrawColor(0, 0, 0, 175)
+	surface.DrawRect(0, 0, w, h)
+
+end
+
+function PANEL:HasRequirements()
+	local inv = LocalPlayer():GetCharacter():GetInventory()
+	local missing = {}
+	for k, v in pairs(self.requirements) do
+		if inv:GetItemCount(k) < v then
+			local i = ix.item.Get(k)
+			missing[#missing + 1] = i.name
+		end
+	end
+
+	if #missing > 0 then
+		return false
+	else
+		return true
+	end
+
+	return true
+end
+
+vgui.Register("CraftingListItem", PANEL, "DPanel")
 
 hook.Add("CreateMenuButtons", "ixCrafting", function(tabs)
 	tabs["crafting"] = function(container)
-		container:Add("ixCrafting")
+		container:Add("CraftingListFrame")
 	end
 end)
